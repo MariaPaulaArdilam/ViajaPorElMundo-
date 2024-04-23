176880a538c7080220eccf39beb547adea97ba86
MariaPaulaArdilam <antonietainfo@gmail.com>
2024-04-23 18:58:03 -0500

    se crea la vista de la pantalla 1

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { OneComponent } from './pages/one/one.component';
 
 const routes: Routes = [
 
-  //crear las rutas 
+  { path: 'one', component: OneComponent }
 ];
 
 @NgModule({
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,13 @@ import { BrowserModule } from '@angular/platform-browser';
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { HeaderComponent } from './components/header/header.component';
+import { OneComponent } from './pages/one/one.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    OneComponent
   ],
   imports: [
     BrowserModule,
